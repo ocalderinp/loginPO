e0806917c93b2597c10da3294e9a98919b431133
oscar calderin <oscar.calderin@abstracta.com.uy>
2019-07-03 22:29:09 -0300

    cambios

--- src/test/java/TestProducts.java
+++ src/test/java/TestProducts.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pageObjects.opencart.HomeOpencart;
 import pageObjects.opencart.ProductItem;
@@ -8,15 +10,22 @@ import pageObjects.opencart.ProductItem;
 import java.util.List;
 
 public class TestProducts {
-
     WebDriver driver;
-    @Test
-    public void verifySimbolTaxes(){
+    HomeOpencart home;
+
+    @BeforeMethod
+    public void setup(){
         System.setProperty("webdriver.chrome.driver",
                 "drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://opencart.abstracta.us/");
-        HomeOpencart home = new HomeOpencart(driver);
+        home = new HomeOpencart(driver);
+    }
+
+
+    @Test
+    public void verifySimbolTaxes(){
+
         List<ProductItem> listadoProductos =
                 home.getProducts();
         for(ProductItem productItem : listadoProductos){
@@ -29,13 +38,13 @@ public class TestProducts {
         }
     }
 
+    @Test
     public void testSearch(){
-        System.setProperty("webdriver.chrome.driver",
-                "drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("http://opencart.abstracta.us/");
-        HomeOpencart home = new HomeOpencart(driver);
-
         home.searchProduct("iMac");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
     }
 }
